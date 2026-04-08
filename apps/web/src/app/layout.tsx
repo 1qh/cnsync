d5de028241c01cb172502bcaec612f4de3497f55
Lai Quang Huy <laiquanghuy24122001@gmail.com>
2026-04-09 00:25:30 +0700

    fix: add suppressHydrationWarning and antialiased

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -4,8 +4,8 @@ import type { ReactNode } from 'react'
 import { cn } from '@a/ui'
 import { mono, sans } from './fonts'
 const Layout = ({ children }: { children: ReactNode }) => (
-  <html className={cn('font-sans', sans.variable, mono.variable)} lang='en'>
-    <body>{children}</body>
+  <html className={cn('font-sans', sans.variable, mono.variable)} lang='en' suppressHydrationWarning>
+    <body className='antialiased'>{children}</body>
   </html>
 )
 export default Layout
